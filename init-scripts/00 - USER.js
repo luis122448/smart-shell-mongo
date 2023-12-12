ba1754d82cd299a2f68df3514b30a94840463b66
luis122448 <luis122448>
2023-12-12 01:33:39 +0000

    Add init-scripts

--- init-scripts/00 - USER.js
+++ init-scripts/00 - USER.js
@@ -1,11 +1,21 @@
 const password = process.env.MONGO_PASSWORD || "default_password"; // Obtener el password de la variable de entorno o utilizar uno por defecto
 db.createUser({
-    user: "user-smart-shell",
+    user: "user_smart_shell",
     pwd: password,
     roles: [
         {
             role: "readWrite",
-            db: "smart-shell"
+            db: "smart_shell"
+        }
+    ]
+});
+db.createUser({
+    user: "developer_smart_shell",
+    pwd: password,
+    roles: [
+        {
+            role: "readWrite",
+            db: "smart_shell"
         }
     ]
 });
