88d5fdd0e8403f94e03882122b7c56575c2d504d
Luis Antonio Calvo Quispe <luis122448@gmail.com>
2025-10-15 23:09:36 -0500

    refactor: mirror changes

--- install.sh
+++ install.sh
@@ -2,9 +2,3 @@
 
 # Create environment variables file
 touch .env
-
-# Delete directories
-sudo rm -rf /var/www/volumes/smart-shell/smart-shell-mongo
-
-# Create directories
-mkdir -p /var/www/volumes/smart-shell/smart-shell-mongo/data
